5df98a1b9fd105410fa10ef35bc93c244b439a9d
Wilker <Wilker.rj@gmail.com>
2016-06-22 18:50:52 -0300

    Adiciona Classe de Controle de Estoque, classe auxilidar para validaçao dos dados inseridos e realizada modificações na classe Estoque

--- Supermercado/src/supermercado/Estoque.java
+++ Supermercado/src/supermercado/Estoque.java
@@ -5,10 +5,36 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public abstract class Estoque {
+public class Estoque {
 
-    Map<Produto, Integer> estoque = new HashMap<>();
+    private Map<Produto, Integer> estoque;
 
     public Estoque() {
-    }  
+        estoque = new HashMap<>();
+    }
+
+    /**
+     * Recebe um Produto e verifica a quantidade dele em estoque.
+     *
+     * @param produto
+     * @return retorna a quantidade em estoque e -1 caso o produto não exista no
+     * estoque
+     */
+    protected int verificaQtdDisponivel(Produto produto) {
+        return estoque.getOrDefault(produto, -1);
+    }
+
+    /**
+     * Recebe um Produto e verifica se existe no estoque
+     *
+     * @param produto
+     * @return true se o produto já está no estoque e falso caso contrário
+     */
+    protected boolean buscaNoEstoque(Produto produto) {
+        return estoque.containsKey(produto);
+    }
+    
+    protected void insereNoEstoque(Produto produto, int quantidade){
+        estoque.put(produto, quantidade);
+    }
 }
