f7af83812fa12f4a4203a3ebc3364921b781af9b
Wilker <Wilker.rj@gmail.com>
2016-07-04 20:28:10 -0300

    Removido acoplamento entre a classe Controle de Estoque e Estoque. Movida lógica do método adicionarAoEstoque para a classe Estoque

--- Supermercado/src/supermercado/Estoque.java
+++ Supermercado/src/supermercado/Estoque.java
@@ -1,16 +1,14 @@
 package supermercado;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
 
 public class Estoque {
 
     static ArrayList<ProdutoEstoque> estoque = new ArrayList<>();
 
     public Estoque() {
-        
+
     }
 
     /**
@@ -21,10 +19,10 @@ public class Estoque {
      * estoque
      */
     protected Integer verificaQtdDisponivel(String nome) {
-        for (ProdutoEstoque estoque : estoque) {
-                if (nome.equalsIgnoreCase(estoque.produto.nome)) {
-                    return estoque.getQuantidadeEstoque();
-                }
+        for (ProdutoEstoque prodEstq : estoque) {
+            if (nome.equalsIgnoreCase(prodEstq.produto.nome)) {
+                return prodEstq.getQuantidadeEstoque();
+            }
         }
         return -1;
     }
@@ -35,40 +33,42 @@ public class Estoque {
      * @param produto
      * @return true se o produto já está no estoque e falso caso contrário
      */
-    static boolean buscaProduto(String nome){
-        for (int i=0; i < estoque.size(); i++){
-            if (nome.equalsIgnoreCase(estoque.get(i).getProdutoEstoque().getNome()))
+    static boolean buscaProduto(String nome) {
+        for (int i = 0; i < estoque.size(); i++) {
+            if (nome.equalsIgnoreCase(estoque.get(i).getProdutoEstoque().getNome())) {
                 return true;
+            }
         }
         return false;
     }
-    
-    static ProdutoEstoque pegaProduto(String nome){
-        for (int i=0; i < estoque.size(); i++){
-            if (nome.equalsIgnoreCase(estoque.get(i).getProdutoEstoque().getNome()))
+
+    static ProdutoEstoque pegaProduto(String nome) {
+        for (int i = 0; i < estoque.size(); i++) {
+            if (nome.equalsIgnoreCase(estoque.get(i).getProdutoEstoque().getNome())) {
                 return estoque.get(i);
+            }
         }
         System.out.println("Produto não encontrado");
         return null;
     }
-    
-    protected void cadastraNoEstoque(Produto produto, int quantidade){
+
+    protected void cadastraNoEstoque(Produto produto, int quantidade) {
         ProdutoEstoque pe = new ProdutoEstoque(produto, quantidade);
         estoque.add(pe);
     }
-    
-    static void darBaixa(ArrayList<ItemCarrinho> carrinho){
+
+    static void darBaixa(ArrayList<ItemCarrinho> carrinho) {
         int quantidade;
-        for (int i = 0; i < carrinho.size(); i++){
-            if (buscaProduto(carrinho.get(i).getProduto().getNome()) == true){
+        for (int i = 0; i < carrinho.size(); i++) {
+            if (buscaProduto(carrinho.get(i).getProduto().getNome()) == true) {
                 quantidade = carrinho.get(i).getQtd();
                 int novaQuantidade = estoque.get(i).getQuantidadeEstoque() - quantidade;
                 pegaProduto(carrinho.get(i).getProduto().getNome()).setQuantidadeEstoque(novaQuantidade);
             }
         }
     }
-    
-    public void relatorioEstoque(){
+
+    public void relatorioEstoque() {
         System.out.println("RELATÓRIO DE ESTOQUE:");
         System.out.println("");
         for (ProdutoEstoque estoque1 : estoque) {
@@ -80,15 +80,26 @@ public class Estoque {
         }
     }
 
-    static void imprimeEstoque(){
+    static void imprimeEstoque() {
         System.out.println("Produtos à venda");
         System.out.println("");
-        for (int i = 0; i < estoque.size(); i++){    
+        for (int i = 0; i < estoque.size(); i++) {
             System.out.println("Produto: " + estoque.get(i).getProdutoEstoque().getNome());
             System.out.println("Preço: R$" + estoque.get(i).getProdutoEstoque().getPreco());
             System.out.println("");
         }
         System.out.println("");
     }
-    
+
+    public void adicionaAoEstoque(String nome, int quantidade) {
+        for (ProdutoEstoque prodEstq : estoque) {
+            if (nome.equalsIgnoreCase(prodEstq.produto.nome)) {
+                if (prodEstq.getQuantidadeInicial() == 0) {
+                    prodEstq.setQuantidadeInicial(quantidade);
+                }
+                prodEstq.setQuantidadeEstoque(prodEstq.getQuantidadeEstoque()
+                        + quantidade);
+            }
+        }
+    }
 }
